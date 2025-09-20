16d7fb505113ae9f03309e9016896b704827ce26
sohaib271 <sohaibsheikh6299@gmail.com>
2025-09-20 21:47:49 +0500

    fd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,8 +10,10 @@ import { useMemo } from "react";
 const Index = () => {
   const navigate=useNavigate();
   const {data:items,isLoading}=useItems();
+  console.log(items)
   const {addButtonController,buttonStateFalse}=useAddItems();
   const {data:booking}=useBookings();
+  console.log(booking)
 
   const isReserved=useMemo(()=>{
     const findB=booking?.find(b=> b.status==="Reserved");
@@ -41,8 +43,8 @@ const Index = () => {
                 className="animate-slide-up"
                 style={{ animationDelay: `${index * 0.2}s` }}
               >
-                <DishCard name={dish.item_name} isReserved={isReserved}  itemId={dish.id} description={dish.description} price={dish.price} image={dish.item_image} rating={4.5}
-                 />
+                {/* <DishCard name={dish.item_name} isReserved={isReserved}  itemId={dish.id} description={dish.description} price={dish.price} image={dish.item_image} rating={4.5}
+                 /> */}
               </div>
             ))}
           </div>
