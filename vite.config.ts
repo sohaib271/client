a041b3211c4908ccf0ddf1c5f7370d04c13283dd
sohaib271 <sohaibsheikh6299@gmail.com>
2025-09-20 15:33:35 +0500

    prisma resolved

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig(() => ({
   plugins: [
     react(),
   ].filter(Boolean),
